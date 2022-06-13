d7120eb208b6edcc68bbf444848fc4952dfcfbbe
onwaying <673975053@qq.com>
2022-06-13 18:41:30 +0800

    add learning Cmake ch2

---
+++ learningCmake/ch2/build/src/CMakeFiles/ch2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/ch2"
+  "../../bin/ch2.pdb"
+  "CMakeFiles/ch2.dir/main.cpp.o"
+  "CMakeFiles/ch2.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ch2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ learningCmake/ch2/src/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main(int argc, char** argv){
+    std::cout<<"c++ hello world lib"<<std::endl;
+    return 0;
+}
